f9d0129517bcd7766e310c70e8df0e14c166704f
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2021-11-06 12:54:59 +0100

    Added link parent

--- datamodel/dv_pipeline_description/view_DVPD_DV_MODEL_TABLE.sql
+++ datamodel/dv_pipeline_description/view_DVPD_DV_MODEL_TABLE.sql
@@ -1,12 +1,15 @@
 drop view dv_pipeline_description.DVPD_DV_MODEL_TABLE cascade;
 create or replace view dv_pipeline_description.DVPD_DV_MODEL_TABLE as 
 select distinct 
-schema_name
-,table_name
-,stereotype
-,hub_key_column_name
-,link_key_column_name
-,diff_hash_column_name
-,satellite_parent_table from dv_pipeline_description.DVPD_DV_MODEL_TABLE_PER_PIPELINE ;
+	schema_name
+	,table_name
+	,stereotype
+	,hub_key_column_name
+	,link_key_column_name
+	,diff_hash_column_name
+	,satellite_parent_table 
+from dv_pipeline_description.DVPD_DV_MODEL_TABLE_PER_PIPELINE ;
+
+-- select * From dv_pipeline_description.DVPD_DV_MODEL_TABLE;
 
 
