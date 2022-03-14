ec73468a1fe055132127c613505d2f18f6579ea7
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2022-03-14 19:47:15 +0100

    Added driving key check

--- datamodel/dv_pipeline_description/tests_and_demos/insert_test03_check_xsat_specifics.sql
+++ datamodel/dv_pipeline_description/tests_and_demos/insert_test03_check_xsat_specifics.sql
@@ -1,11 +1,11 @@
 
-DELETE FROM dv_pipeline_description.dvpd_dictionary where pipeline_name = 'test03_check_field_target_table';
+DELETE FROM dv_pipeline_description.dvpd_dictionary where pipeline_name = 'test03_check_xsat_specifics';
 INSERT INTO dv_pipeline_description.dvpd_dictionary
 (pipeline_name, dvpd_json)
 VALUES
-('test03_check_field_target_table','{
+('test03_check_xsat_specifics','{
 	"DVPD_Version": "1.0",
-	"pipeline_name": "test03_check_field_target_table",
+	"pipeline_name": "test03_check_xsat_specifics",
 	"record_source_name_expression": "dvpd implementation test",
 	"data_extraction": {
 		"fetch_module_name":"none - this is a pure generator test case"
@@ -16,7 +16,7 @@ VALUES
 		 	  ,{"field_name": "F2_BK_BBB_DECIMAL",	"technical_type": "Decimal(20,0)",	"targets": [{"table_name": "rtjj_03_bbb_hub"}]}
 		 	  ,{"field_name": "F3_AAA_SP1_VARCHAR",	"technical_type": "VARCHAR(200)",	"targets": [{"table_name": "rtjj_03_aaa_p1_sat"}]}
 			  ,{"field_name": "F4_AAA_SP1_DECIMAL",	"technical_type": "DECIMAL(5,0)",	"targets": [{"table_name": "rtjj_03_aaa_p1_sat"}]}
-			  ,{"field_name": "F5_XXX_BAD_FIELD_FOR_ESAT_XXX",	"technical_type": "VARCHAR(200)",	"targets": [{"table_name": "rtjj_03_aaa_bbb_esat"}]}
+			  ,{"field_name": "F5_XXX_BAD_FIELD_FOR_ESAT_XXX",	"technical_type": "VARCHAR(200)",	"targets": [{"table_name": "rtjj_03_aaa_bbb_p1_esat"}]}
 			 ],
 	"data_vault_model": [
 		{"schema_name": "rvlt_test_jj", 
@@ -25,8 +25,10 @@ VALUES
 				,{"table_name": "rtjj_03_aaa_p1_sat",	"stereotype": "sat","satellite_parent_table": "rtjj_03_aaa_HUB","diff_hash_column_name": "RH_rtjj_03_aaa_P1_SAT"}
 				,{"table_name": "rtjj_03_bbb_hub",		"stereotype": "hub","hub_key_column_name": "HK_rtjj_03_bbb"}
 				,{"table_name": "rtjj_03_aaa_bbb_lnk",	"stereotype": "lnk","link_key_column_name": "LK_rtjj_03_aaa_bbb",
-																				"link_parent_tables": ["rvlt_test_jj","rtjj_03_aaa_hub","rtjj_03_bbb_hub"]}
-				,{"table_name": "rtjj_03_aaa_bbb_esat",	"stereotype": "esat","satellite_parent_table": "rtjj_03_aaa_bbb_lnk"}
+																				"link_parent_tables": ["rtjj_03_aaa_hub","rtjj_03_bbb_hub"]}
+				,{"table_name": "rtjj_03_aaa_bbb_p1_esat",	"stereotype": "esat","satellite_parent_table": "rtjj_03_aaa_bbb_lnk"}
+				,{"table_name": "rtjj_03_aaa_bbb_p2_esat",	"stereotype": "esat","satellite_parent_table": "rtjj_03_aaa_bbb_lnk"
+																				 ,"driving_keys": ["HK_XXX_NOT_IN_LINK_XXX"]}	
 				]
 		}
 	]
--- datamodel/dv_pipeline_description/view_DVPD_CHECK_ALL.sql
+++ datamodel/dv_pipeline_description/view_DVPD_CHECK_ALL.sql
@@ -10,7 +10,7 @@ UNION
 select * FROM dv_pipeline_description.dvpd_check_model_stereotype_and_parameters
 where message <>'ok'
 UNION
-select * FROM dv_pipeline_description.dvpd_check_esat_specifics  
+select * FROM dv_pipeline_description.dvpd_check_xsat_specifics  
 where message <>'ok'
 UNION
 select * FROM dv_pipeline_description.dvpd_check_hub_specifics  
