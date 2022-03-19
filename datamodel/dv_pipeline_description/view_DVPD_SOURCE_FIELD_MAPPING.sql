8a36dd687ac5857ad3474f525b5f2ba859c64162
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2022-03-19 11:13:15 +0100

    Removed process block from source field mapping

--- datamodel/dv_pipeline_description/view_DVPD_SOURCE_FIELD_MAPPING.sql
+++ datamodel/dv_pipeline_description/view_DVPD_SOURCE_FIELD_MAPPING.sql
@@ -31,14 +31,13 @@ select
 ,json_array_elements(targets)->'hash_cleansing_rules' as hash_cleansing_rules
 from source_fields
 )
-,field_group_expansion_and_normalization AS (
 select 
  lower(pipeline) as  pipeline
-,case when field_groups is not null then json_array_elements_text(field_groups) else '_A_' end as field_group
 ,upper(field_name) as field_name
 ,upper(field_type) as field_type
 ,lower(target_table) as target_table
 ,upper(coalesce (target_column_name,field_name)) as target_column_name
+,case when field_groups is not null then json_array_elements_text(field_groups) else '_A_' end as field_group
 ,upper(coalesce(hierarchy_key_suffix,'')) as hierarchy_key_suffix
 ,upper(coalesce (target_column_type,field_type)) as target_column_type
 ,coalesce(to_number(prio_in_key_hash,'9'),0) as prio_in_hashkey
@@ -49,36 +48,7 @@ select
 ,parsing_expression
 ,hash_cleansing_rules
 from target_expansion
-)
-,hierachy_partner_field as (
-select 
-	origin.pipeline
-	,origin.field_group
-	,origin.target_table
-	,origin.target_column_name
-	,origin.field_name origin_field_name
-	,partner.field_name partner_field_name
-	,partner.hierarchy_key_suffix partner_hierarchy_key_suffix
-from field_group_expansion_and_normalization origin
-join field_group_expansion_and_normalization partner on partner.pipeline=origin.pipeline
-							and partner.field_group=origin.field_group
-							and partner.target_table=origin.target_table
-							and partner.target_column_name = origin.target_column_name
-							and partner.field_name <>origin.field_name
-where partner.hierarchy_key_suffix <>''
-)
--- finale structure (adding process block information)
-select 
- fgean.*
- ,case when hierarchy_key_suffix <>''  then replace(fgean.field_group,'_A_','')||'H1_'||hierarchy_key_suffix
- 	   when partner_hierarchy_key_suffix is not null and partner_hierarchy_key_suffix <> '' 	
- 	   									then replace(fgean.field_group,'_A_','')||'H0_'||partner_hierarchy_key_suffix
- 	   else fgean.field_group end process_block
-from field_group_expansion_and_normalization fgean
-left join hierachy_partner_field hpf on  hpf.pipeline=fgean.pipeline
-							and hpf.field_group=fgean.field_group
-							and hpf.target_table=fgean.target_table
-							and hpf.target_column_name = fgean.target_column_name;
+
 
 -- select * from dv_pipeline_description.DVPD_SOURCE_FIELD_MAPPING order by pipeline ,field_name,target_table;
 
