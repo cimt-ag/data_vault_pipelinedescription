66f69a3f81a99cefa512f658a75cb15a28566ac5
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2022-03-19 14:21:01 +0100

    Sicherung

--- datamodel/dv_pipeline_description/view_DVPD_PIPELINE_PROCESS_STAGE_TO_DV_MODEL_MAPPING.sql
+++ datamodel/dv_pipeline_description/view_DVPD_PIPELINE_PROCESS_STAGE_TO_DV_MODEL_MAPPING.sql
@@ -4,18 +4,25 @@ create or replace view dv_pipeline_description.DVPD_PIPELINE_PROCESS_STAGE_TO_DV
 
 select distinct 
 	ppp.pipeline
+	,ppp.process_block 
 	,ppp.table_name
 	,dmc.column_name 
 	,dmc.dv_column_class  
 	,case when process_block ='_A_' then dmc.column_name 
 	 else dmc.column_name||'_'||process_block end stage_column_name
 	,dmc.column_type 
-	,ppp.process_block 
+	,dmc.column_block 
 	,ppp.field_group
 	,ppp.hierarchy_key_suffix 
+	,pfte.field_name 
+	,pfte.field_type 
+	,pfte.is_encrypted 
 from dv_pipeline_description.dvpd_pipeline_process_plan ppp
 join dv_pipeline_description.dvpd_dv_model_column dmc on dmc.table_name=ppp.table_name 
-												and dmc.dv_column_class not in ('meta');
+												and dmc.dv_column_class not in ('meta')
+left join dv_pipeline_description.dvpd_pipeline_field_target_expansion pfte on pfte.pipeline =ppp.pipeline 
+																		and pfte.target_table =dmc.table_name 
+																		and pfte.target_column_name =dmc.column_name 
 
 
 -- select * from dv_pipeline_description.DVPD_PIPELINE_PROCESS_STAGE_TO_DV_MODEL_MAPPING order by pipeline,table_name,process_block;										
--- datamodel/dv_pipeline_description/view_DVPD_PIPELINE_STAGE_TABLE_COLUMN.sql
+++ datamodel/dv_pipeline_description/view_DVPD_PIPELINE_STAGE_TABLE_COLUMN.sql
@@ -8,32 +8,22 @@ create or replace view dv_pipeline_description.DVPD_PIPELINE_STAGE_TABLE_COLUMN 
 
 with pipelines AS(
 select distinct pipeline 
-from dv_pipeline_description.dvpd_source_field
+from dv_pipeline_description.dvpd_pipeline_target_table
 )
 select distinct 
-	plap.pipeline 
-	,coalesce (sfm.field_name,mc.column_name )   stage_column_name
-	,mc.column_type 
-	,min(mc.column_block) column_block
-	,sfm.field_name 
-	,sfm.field_type 
-	,coalesce(sfm.is_encrypted ,false) encrypt
+	pipeline 
+	,stage_column_name
+	,column_type 
+	,min(column_block) column_block
 	,false is_meta
-from dv_pipeline_description.dvpd_pipeline_leaf_and_process_table plap 
-join dv_pipeline_description.dvpd_dv_model_column mc on mc.table_name = plap.table_to_process 
-													 and mc.dv_column_class not in ('meta')
-left join dv_pipeline_description.DVPD_PIPELINE_FIELD_TARGET_EXPANSION sfm on sfm.target_table = plap.table_to_process 	
-													  and sfm.target_column_name =mc.column_name
-group by 1,2,3,5,6,7
+from  dv_pipeline_description.dvpd_pipeline_process_stage_to_dv_model_mapping
+group by 1,2,3,5
 union 
 select
 	pipeline 
 	,dmcl.meta_column_name 
 	,dmcl.meta_column_type 
 	, 1
-	,null 
-	,null
-	,false
 	,true is_meta
 from pipelines 
 join dv_pipeline_description.dvpd_meta_column_lookup dmcl on dmcl.stereotype ='_stg' ;
