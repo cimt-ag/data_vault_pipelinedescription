38a4c255ee62f5f3993549424da42a96a2b64698
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2022-03-20 14:12:14 +0100

    Added test27, dependend child key

--- datamodel/dv_pipeline_description/utilty_scripts/render_atmtst_reference_data_array.sql
+++ datamodel/dv_pipeline_description/utilty_scripts/render_atmtst_reference_data_array.sql
@@ -1,15 +1,15 @@
 /* DV_MODEL_COLUMN REFERENCE DATA ARRAY */
 select '["' 
- || dmtpp.schema_name  || '","'
+ || dptt.schema_name  || '","'
  || dmc.table_name || '",'
  || coalesce(dmc.column_block,-1) || ',"'
  || coalesce(dmc.dv_column_class,'') || '","'
  || coalesce(dmc.column_name,'') || '","'
  || coalesce(dmc.column_type,'') || '"],'
-from dv_pipeline_description.dvpd_dv_model_table_per_pipeline dmtpp
-join dv_pipeline_description.dvpd_dv_model_column dmc  on  dmc.table_name =dmtpp.table_name 
+from dv_pipeline_description.dvpd_pipeline_target_table dptt  
+join dv_pipeline_description.dvpd_dv_model_column dmc  on  dmc.table_name =dptt.table_name 
    													and dmc.dv_column_class  <> 'meta'											
-where dmtpp.pipeline like 'test80%'  													
+where dptt.pipeline like 'test27%'  													
 
 
 
@@ -21,8 +21,8 @@ select '["'
  || coalesce(column_block,-1) || ','
  || coalesce('"'||field_name||'"','null') || ','
  || coalesce('"'||field_type||'"','null') || ','
- || coalesce(encrypt::varchar,'null') || '],'
-from dv_pipeline_description.dvpd_pipeline_stage_table_column									
+ || coalesce(is_encrypted::varchar,'false') || '],'
+from dv_pipeline_description.DVPD_PIPELINE_STAGE_TABLE_COLUMN								
 where not is_meta   
-and   pipeline like 'test80%'  													
+and   pipeline like 'test27%'  													
 
