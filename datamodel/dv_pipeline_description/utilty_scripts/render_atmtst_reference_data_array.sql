f3b14dad2a52ba60a21406dd5bc9f3020ad5341d
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2022-04-25 16:27:34 +0200

    Added test 32 driving key on dependent child

--- datamodel/dv_pipeline_description/utilty_scripts/render_atmtst_reference_data_array.sql
+++ datamodel/dv_pipeline_description/utilty_scripts/render_atmtst_reference_data_array.sql
@@ -1,7 +1,7 @@
 with target as (
 select distinct pipeline_name
 from dv_pipeline_description.dvpd_pipeline_DV_table
-where pipeline_name like 'test31%'
+where pipeline_name like 'test32%'
 )
 select 1 block
 ,1 reverse_order
