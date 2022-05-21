dc71cade17ed2900602468d1725362c4caeb1623
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2022-05-21 18:47:33 +0200

    Check of xenc relations final

--- datamodel/dv_pipeline_description/tests_and_demos/insert_xenc_test01_check_bad_reference.sql
+++ datamodel/dv_pipeline_description/tests_and_demos/insert_xenc_test01_check_bad_reference.sql
@@ -12,23 +12,29 @@ VALUES
 	},
 
 	"fields": [
-		      {"field_name": "F1_BK_AAA_ENCRYPT_ME", 	"field_type": "Varchar(20)"
+		      {"field_name": "F1_BK_AAA_ENCRYPT_ME", 	"field_type": "Varchar(20)", "needs_encryption":true
 														,"targets": [{"table_name": "rxecd_01_aaa_hub"}]}
 		 	  ,{"field_name": "F2_AAA_SP1_VARCHAR"	,		"field_type": "VARCHAR(200)"
 														,"targets": [{"table_name": "rxecd_01_aaa_sat"}]}
-		 	  ,{"field_name": "F3_AAA_SP1_ENCRYPT_ME",	"field_type": "VARCHAR(200)", "needs_encryption":true
-														,"targets": [{"table_name": "rxecd_01_aaa_sat"}]}
+		 	  ,{"field_name": "F3_AAA_SP1_VARCHAR",	"field_type": "VARCHAR(200)"
+														,"targets": [{"table_name": "rxecd_01_aaa_no_encrpytion_sat"}]}
+		 	  ,{"field_name": "F4_AAA_SP2_ENCRYPT_ME",	"field_type": "VARCHAR(200)", "needs_encryption":true
+														,"targets": [{"table_name": "rxecd_01_aaa_p2_sat"}]}
 			 ],
 	"data_vault_model": [
 		{"schema_name": "rvlt_xenc_data", 
 		 "tables": [
 				{"table_name": "rxecd_01_aaa_hub",		"stereotype": "hub","hub_key_column_name": "HK_rxecd_01_aaa"}
-				,{"table_name": "rxecd_01_aaa_sat",		"stereotype": "sat","satellite_parent_table": "rxecd_01_aaa_hub","diff_hash_column_name": "RH_rxecd_01_aaa_sat"}
+				,{"table_name": "rxecd_01_aaa_no_encrpytion_sat",		"stereotype": "sat","satellite_parent_table": "rxecd_01_aaa_hub","diff_hash_column_name": "RH_rxecd_01_aaa_no_encrpytion_sat"}
+				,{"table_name": "rxecd_01_aaa_p2_sat",		"stereotype": "sat","satellite_parent_table": "rxecd_01_aaa_hub","diff_hash_column_name": "RH_rxecd_01_aaa_p2_sat"}
 				]
 		}
 		,{"schema_name": "rvlt_xenc_keys", 
 		 "tables": [
-				{"table_name": "rxeck_01_aaa_sat_ek",	"stereotype": "xenc_sat-ek", "xenc_content_table_name":"rxecd_xxxnot_existingxxx"}
+				{"table_name": "rxeck_01_bad_reference_ek",	"stereotype": "xenc_sat-ek", "xenc_content_table_name":"rxecd_xxxnot_existingxxx"}
+				,{"table_name": "rxeck_01_no_encryption_ref_ek",	"stereotype": "xenc_sat-ek", "xenc_content_table_name":"rxecd_01_aaa_no_encrpytion_sat"}
+				,{"table_name": "rxeck_01_1of2_ek",	"stereotype": "xenc_sat-ek", "xenc_content_table_name":"rxecd_01_aaa_p2_sat"}
+				,{"table_name": "rxeck_01_2of2_ek",	"stereotype": "xenc_sat-ek", "xenc_content_table_name":"rxecd_01_aaa_p2_sat"}
 				]
 		}
 	]
---
+++ datamodel/dv_pipeline_description/view_XENC_CHECK_RELATIONS.sql
@@ -0,0 +1,66 @@
+-- drop view if exists dv_pipeline_description.XENC_CHECK_RELATIONS cascade;
+create or replace view dv_pipeline_description.XENC_CHECK_RELATIONS as
+
+with target_tables_with_encrypted_columns as (
+select  distinct 
+	pfte.pipeline_name 
+	, pfte.target_table table_name
+from dv_pipeline_description.dvpd_pipeline_field_properties pfp 
+join dv_pipeline_description.dvpd_pipeline_field_target_expansion pfte on pfte.pipeline_name =pfp.pipeline 
+																	and pfte.field_name = pfp.field_name 
+where pfp.needs_encryption 
+)
+, check_content_table_relation as (
+select
+  epdtp.pipeline_name
+  ,'Table'::TEXT  object_type 
+  ,epdtp.table_name object_name
+  ,'XENC_CHECK'::text  check_ruleset
+  ,case when cpdt.table_name is null then 'Unknown xenc_content_table_name: '|| epdtp.xenc_content_table_name  
+  		when ttwec.table_name is null then 'Target table has no encrypted columns:' || epdtp.xenc_content_table_name
+    	else 'ok' 
+    end  message
+from dv_pipeline_description.xenc_pipeline_dv_table_properties_raw epdtp
+left join  dv_pipeline_description.dvpd_pipeline_dv_table cpdt on cpdt.pipeline_name = epdtp.pipeline_name  
+															 and cpdt.table_name = epdtp.xenc_content_table_name 
+left join target_tables_with_encrypted_columns  ttwec on ttwec.pipeline_name = epdtp.pipeline_name 
+													 and ttwec.table_name = epdtp .xenc_content_table_name														 
+)
+, count_encryption_table_coverage as (
+select
+  ttwec.pipeline_name
+  ,ttwec.table_name 
+  ,count(1) coverage_count
+  ,string_agg(epdtp.table_name,',') covering_table_names
+from dv_pipeline_description.xenc_pipeline_dv_table_properties_raw epdtp
+join target_tables_with_encrypted_columns  ttwec on ttwec.pipeline_name = epdtp.pipeline_name 
+													 and ttwec.table_name = epdtp .xenc_content_table_name
+group by 1,2													 
+)
+, check_encrytion_coverage_content as (
+select
+  ttwec.pipeline_name
+  ,'Table'::TEXT  object_type 
+  ,ttwec.table_name object_name
+  ,'XENC_CHECK'::text  check_ruleset
+  ,case when cetc.coverage_count is null then 'Table with encryted data has no partner'
+  		when cetc.coverage_count > 1 then 'Table is covered by multiple encryption tables:'|| cetc.covering_table_names
+    	else 'ok' 
+    end  message
+from target_tables_with_encrypted_columns ttwec
+left join count_encryption_table_coverage  cetc on cetc.pipeline_name = ttwec.pipeline_name 
+												and cetc.table_name = ttwec .table_name 														 
+)
+select * from check_content_table_relation
+union
+select * from check_encrytion_coverage_content
+
+													 ;
+
+comment on view dv_pipeline_description.XENC_CHECK_RELATIONS IS
+	'Check for encryption extention specific relations';
+
+-- select * from dv_pipeline_description.XENC_CHECK_RELATIONS order by 1,2,3
+
+
+
