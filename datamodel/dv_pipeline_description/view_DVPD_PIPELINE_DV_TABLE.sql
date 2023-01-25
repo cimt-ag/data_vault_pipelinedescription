f6baa52cfb9d61d71fa30152909ab15cc267a41c
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2023-01-25 11:09:07 +0100

    Adde "is_endated_default" to model_profile. Using the property during table definition cleansing

--- datamodel/dv_pipeline_description/view_DVPD_PIPELINE_DV_TABLE.sql
+++ datamodel/dv_pipeline_description/view_DVPD_PIPELINE_DV_TABLE.sql
@@ -30,11 +30,13 @@ select
 , upper(diff_hash_column_name) as diff_hash_column_name
 , lower(satellite_parent_table) as satellite_parent_table
 , coalesce(is_link_without_sat::bool,false) as is_link_without_sat
-, coalesce(is_enddated ::bool,true) as is_enddated 
+, coalesce(is_enddated ::bool,mp_is_endated_default.property_value ::bool) as is_enddated 
 , lower(coalesce( pdt.model_profile_name,pp.model_profile_name )) model_profile_name
 , table_content_comment
 from dv_pipeline_description.dvpd_pipeline_dv_table_raw pdt
 join dv_pipeline_description.dvpd_pipeline_properties pp on pp.pipeline_name =lower(pdt.pipeline_name )
+left join dv_pipeline_description.dvpd_model_profile mp_is_endated_default on mp_is_endated_default.model_profile_name = lower(coalesce( pdt.model_profile_name,pp.model_profile_name )) 
+													   and mp_is_endated_default.property_name ='is_enddated_default'
 ;
 
 comment on view dv_pipeline_description.DVPD_PIPELINE_DV_TABLE is
