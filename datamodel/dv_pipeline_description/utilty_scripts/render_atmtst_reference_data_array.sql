eec2a9f56f9f7b22317356ac5fbe454709ae6cbb
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2023-05-25 11:18:52 +0200

    aligned Resursion names

--- datamodel/dv_pipeline_description/utilty_scripts/render_atmtst_reference_data_array.sql
+++ datamodel/dv_pipeline_description/utilty_scripts/render_atmtst_reference_data_array.sql
@@ -21,7 +21,7 @@
 with target as (
 select distinct pipeline_name
 from dv_pipeline_description.dvpd_pipeline_DV_table
-where pipeline_name like 'test61%'
+where pipeline_name like 'test82%'
 ) /* */
 select 1 block
 ,1 reverse_order
