b2ff7e80d32a6911e015bd266529c380b5f784c2
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2023-09-01 11:00:50 +0200

    Added far future function to DDL scripts

---
+++ datamodel/libdwh/schema_LIBDWH.sql
@@ -0,0 +1,11 @@
+
+
+-- DROP SCHEMA "libdwh";
+
+CREATE schema if not EXISTS "libdwh";
+
+COMMENT ON SCHEMA "libdwh"
+  IS 'Schema containg functions and procedures for data warehouse processing and querying';
+
+
+
