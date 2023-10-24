ec87867073a3a80df80ade30f54779446c464f48
Joscha von Hein <jvonhein@cimt-ag.de>
2023-10-24 15:58:44 +0200

    table style

--- processes/render_documentation/__main__.py
+++ processes/render_documentation/__main__.py
@@ -11,16 +11,21 @@ def parse_json_file(file_path):
     except json.JSONDecodeError as e:
         return f"Error parsing JSON: {str(e)}"
 
+# catch the usual suspects
+def is_json_true(json):
+    return json==True or json=="true" or json=="True"
+
 def parse_target(target):
-    print(f"parse({target}):")
+#    print(f"parse({target}):")
     result = target["table_name"]
     
     result += f".[{target['column_name']}]" if 'column_name' in target else ""
 
     in_brackets = []
-    if 'exclude_from_key_hash' in target:  
+    if 'exclude_from_key_hash' in target and is_json_true(target['exclude_from_key_hash']):
+        # print(f"exclude_from_key_hash: {target['exclude_from_key_hash']}")  
         in_brackets.append("not in key hash") 
-    if 'exclude_from_change_detection' in target:
+    if 'exclude_from_change_detection' in target and is_json_true(target['exclude_from_change_detection']):
         in_brackets.append("not in diff hash") 
     if 'relation_names' in target:
         relation_names = target["relation_names"] 
@@ -32,14 +37,20 @@ def parse_target(target):
     if len(in_brackets) > 0:
         result += " (" + ", ".join(in_brackets) + ")"
     
-    print(f"result: {result}")
+#    print(f"result: {result}")
     return result
 
 
 
 def create_documentation(pipeline_name, fields):
     html = "<!DOCTYPE html>\n<html>\n"
-    html += f"<head>\n\t<title>{pipeline_name}</title>\n</head>"
+    html += "<head>\n"
+    html += "<style>\n"
+    html += "table, th, td {border: 1px solid;}\n"
+    html += "table {border-collapse: collapse;}\n"
+    html += "</style>\n"
+    html += "   <title>{pipeline_name}</title>\n"
+    html += "</head>\n"
     html += "<body>\n"
     html += "   <table>\n"
     html += "       <tr>\n"
@@ -55,7 +66,6 @@ def create_documentation(pipeline_name, fields):
         html += "       <tr>\n"
         html += f"           <td>{field_name}</td>\n"
         html += f"           <td>{field_type}</td>\n"
-        print(f"len(targets): {len(targets)}")
         if len(targets) == 1:
             html += f"           <td>{parse_target(targets[0])}</td>\n"
         else:
@@ -78,10 +88,9 @@ def main():
 
     (pipeline_name, fields) = parse_json_file(args.file_path)
     if isinstance(fields, (dict, list)):
-        print("Parsed JSON data:")
         html = create_documentation(pipeline_name,fields)
         print(html)
-        with open("processes/render_documentation/output.html", "w") as file:
+        with open(f"processes/render_documentation/{pipeline_name}.html", "w") as file:
             file.write(html)
     else:
         print(fields)
