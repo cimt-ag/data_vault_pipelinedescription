161dd31cd6a5ca0a54556b5a18fc050c4cf78ba7
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2025-04-25 20:00:18 +0200

    Added annotation of exclude include json path to devesheet
    (cherry picked from commit 3b5d4cb5f849b053a44ba3212265c7c992e7bbc0)

--- processes/render_dev_sheet/main.py
+++ processes/render_dev_sheet/main.py
@@ -291,7 +291,13 @@ def render_dev_cheat_sheet(dvpi_filepath, documentation_directory, stage_column_
                         g_report_file.write(
                             f"       {field['json_path'].ljust(max_jsonpath_length)}->")
                 g_report_file.write(
-                    f"       {field['field_name'].ljust(max_name_length)}  {field['field_type']}\n")
+                    f"        {field['field_name'].ljust(max_name_length)}  {field['field_type']}\n")
+                if "json_path_excludes" in field:
+                    g_report_file.write(
+                        f"            Exclude json paths: '{'\',\''.join(field['json_path_excludes']) }'\n")
+                if "json_path_includes" in field:
+                    g_report_file.write(
+                        f"            Include only json paths: '{'\',\''.join(field['json_path_includes']) }'\n")
 
 
             g_report_file.write("\n\n------------------------------------------------------\n")
